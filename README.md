f5cf30ae09cb53dff0ce9d967864087ac30a1c4c
Akshay Ram <akshay.ram@glovoapp.com>
2022-05-27 11:57:22 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue 2, vue 3, NX monorepo
+
+POC for vue2, vue3 monorepo with nx
---
+++ apps/vue-2-app/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
